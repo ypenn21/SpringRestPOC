5bedd59a19bd447a45c587977b9e5651d5162c16
ypenn21 <yannipeng21@gmail.com>
2020-11-02 14:10:09 -0800

    first commit

---
+++ src/main/java/com/redhat/poc/dao/HatDAO.java
@@ -0,0 +1,28 @@
+package com.redhat.poc.dao;
+
+import com.redhat.poc.model.Hat;
+
+// put logic get connection and query db in here
+public class HatDAO {
+
+    public Hat createHat(Hat hat){
+        return new Hat();
+    }
+
+
+    public Hat getHat(Integer id){
+
+
+        return new Hat();
+
+    }
+
+    public void deleteHat(Integer id){
+    }
+
+    public Hat updateHat(Hat hat){
+
+
+        return hat;
+    }
+}
